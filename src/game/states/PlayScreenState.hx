4e3419f3912c9e96bacee0f22fcceca89ce4fbb7
Anders Nissen <andnis@gmail.com>
2015-03-18 22:17:45 +0100

    Visualize minions

--- src/game/states/PlayScreenState.hx
+++ src/game/states/PlayScreenState.hx
@@ -5,6 +5,7 @@ import luxe.Color;
 import luxe.Input.KeyEvent;
 import luxe.Input.Key;
 import luxe.Scene;
+import luxe.Sprite;
 import luxe.States;
 import luxe.Text;
 import luxe.tween.Actuate;
@@ -14,10 +15,12 @@ import luxe.Visual;
 class PlayScreenState extends State {
     var scene :Scene;
     var background :Visual;
+    var game :core.Game;
 
     public function new() {
         super({ name: 'PlayScreenState' });
         scene = new Scene('PlayScreenScene');
+        game = tests.SimpleTestGame.create_game();
     }
 
     override function init() {
@@ -39,6 +42,11 @@ class PlayScreenState extends State {
         setup();
     }
 
+    function tile_to_pos(x, y) :Vector {
+        var tileSize = 140;
+        return new Vector(180 + tileSize / 2 + x * (tileSize + 10), 20 + tileSize / 2 + y * (tileSize + 10));
+    }
+
     function setup() {
         background = new Visual({
             pos: new Vector(0, 0),
@@ -47,14 +55,15 @@ class PlayScreenState extends State {
             scene: scene
         });
 
+        var boardSize = game.get_board_size();
         var tileSize = 140;
         Actuate
             .tween(background.color, 0.3, { h: 240, s: 0.5, v: 0.7 })
             .onComplete(function() {
-                for (y in 0 ... 4) {
-                    for (x in 0 ... 4) {
-                        var tile = new luxe.Sprite({
-                            pos: new Vector(180 + tileSize / 2 + x * (tileSize + 10), 20 + tileSize / 2 + y * (tileSize + 10)),
+                for (y in 0 ... boardSize.y) {
+                    for (x in 0 ... boardSize.x) {
+                        var tile = new Sprite({
+                            pos: tile_to_pos(x, y),
                             color: new ColorHSV(360 * Math.random(), 0.5, 0.5),
                             size: new Vector(tileSize, tileSize),
                             scale: new Vector(0, 0),
@@ -63,11 +72,31 @@ class PlayScreenState extends State {
                         tile.rotation_z = -25 + 50 * Math.random();
                         Actuate
                             .tween(tile, 0.2, { rotation_z: 0 })
-                            .delay((y * 4 + x) / 20);
+                            .delay((y * boardSize.x + x) / 20);
                         Actuate
                             .tween(tile.scale, 0.2, { x: 1, y: 1 })
-                            .delay((y * 4 + x) / 20);
+                            .delay((y * boardSize.x + x) / 20);
                     }
+                }
+
+                var minions = game.get_minions();
+                for (m in minions) {
+                    var pos = game.get_minion_pos(m);
+                    var minion = new Visual({
+                        pos: tile_to_pos(pos.x, pos.y),
+                        color: new ColorHSV(100 * m.player.id, 0.8, 0.8),
+                        geometry: Luxe.draw.circle({ r: 60 }),
+                        scene: scene
+                    });
+                    new Text({
+                        text: m.name,
+                        color: new Color(1, 1, 1, 1),
+                        align: TextAlign.center,
+                        align_vertical: TextAlign.center,
+                        point_size: 20,
+                        scene: scene,
+                        parent: minion
+                    });
                 }
             });
     }
