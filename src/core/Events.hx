7b6bfbc76ac5d1e0cc76e1df430d75422060f8f8
Anders Nissen <andnis@gmail.com>
2015-04-05 19:25:56 +0200

    More events

--- src/core/Events.hx
+++ src/core/Events.hx
@@ -4,8 +4,11 @@ package core;
 typedef MinionMovedData = { minionId :Int, from :Point, to :Point };
 typedef MinionAttackedData = { minionId :Int, victimId :Int };
 typedef MinionDiedData = { minionId :Int };
+typedef MinionEnteredData = { minionId :Int };
+typedef PlayerEnteredData = { playerId :Int };
 
 enum Event {
+    GameStarted;
     TurnStarted;
     TurnEnded;
     GameOver;
@@ -15,8 +18,8 @@ enum Event {
     MinionDied(data :MinionDiedData);
     MinionAttacked(data :MinionAttackedData);
 
-    PlayerEntered; // also triggered on game start 
-    MinionEntered; // also triggered on game start (minions should be added *after* initial setup)
+    PlayerEntered(data :PlayerEnteredData); // also triggered on game start 
+    MinionEntered(data :MinionEnteredData); // also triggered on game start (minions should be added *after* initial setup)
 }
 
 typedef Events = Array<Event>;
