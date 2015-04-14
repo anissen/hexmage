61e8a453c2f6ac3fcdebfa01b0b036e490de0ebf
Anders Nissen <andnis@gmail.com>
2015-04-14 19:44:26 +0200

    Press Enter to end turn

--- src/game/states/PlayScreenState.hx
+++ src/game/states/PlayScreenState.hx
@@ -391,6 +391,7 @@ class PlayScreenState extends State {
 
     override function onkeyup(e :KeyEvent) {
         switch (e.keycode) {
+            case Key.enter: { trace('End Turn triggered!'); game.end_turn(); }
             case Key.key_r: reset();
             case Key.escape: Luxe.shutdown(); //Main.switch_to_state('TitleScreenState');
         }
