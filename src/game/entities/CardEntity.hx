ee9d8f2f2216e0444e37695efd3da0dfc06f8e46
Anders Nissen <andnis@gmail.com>
2015-05-05 19:12:13 +0200

    Tweaks to card entities

--- src/game/entities/CardEntity.hx
+++ src/game/entities/CardEntity.hx
@@ -22,6 +22,7 @@ class CardEntity extends Sprite {
     var text :Text;
     var cardWidth :Int = 140;
     var cardHeight :Int = 200;
+    var cardMargin :Int = 8;
 
     public function new(options :CardOptions) {
         var colorHue = 250 + Math.random() * 50;
@@ -36,8 +37,8 @@ class CardEntity extends Sprite {
 
         new Sprite({
             pos: new Vector(cardWidth / 2, cardHeight / 2),
-            size: new Vector(cardWidth - 10, cardHeight - 10),
-            color: new ColorHSV(colorHue, 0.8, 1),
+            size: new Vector(cardWidth - cardMargin, cardHeight - cardMargin),
+            color: new ColorHSV(colorHue, 0.8, 0.9),
             scene: options.scene,
             depth: options.depth,
             parent: this
@@ -45,11 +46,11 @@ class CardEntity extends Sprite {
 
         text = new Text({
             text: '${card.name}', //  (${card.cost})
-            pos: new Vector(cardWidth / 2, 20),
+            pos: new Vector(cardWidth / 2, cardMargin + 15),
             color: new Color(1, 1, 1, 1),
             align: TextAlign.center,
             align_vertical: TextAlign.center,
-            point_size: 20,
+            point_size: 18,
             scene: options.scene,
             parent: this,
             depth: options.depth
