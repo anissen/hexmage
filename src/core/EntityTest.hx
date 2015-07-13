1418c118354493e6dcca0ef2dd2c690dfd536296
Anders Nissen <andnis@gmail.com>
2015-07-13 22:04:17 +0200

    More entity test

--- src/core/EntityTest.hx
+++ src/core/EntityTest.hx
@@ -1,5 +1,48 @@
-
 using Lambda;
+
+/*
+class NEW1_037:
+    events = [
+        OWN_TURN_END.on(Buff(RANDOM_OTHER_FRIENDLY_MINION, "NEW1_037e"))
+    ]
+*/
+
+//typedef Buff = {  }
+
+enum Action {
+    EndTurn;
+    Effect(tag :Tag, value :Int);
+}
+
+enum Event {
+    TurnEnded;
+}
+
+class Game {
+    var event_listeners :Map<Event, Void->Action>;
+    
+    public function new() {
+        event_listeners = new Map();
+    }
+    
+    public function do_action(action :Action) {
+        switch (action) {
+            case EndTurn: emit_event(TurnEnded);
+            case _:
+        }
+    }
+    
+    public function on_event(event :Event, func :Void->Action) {
+        event_listeners[event] = func;
+    }
+    
+    public function emit_event(event :Event) {
+        if (event_listeners.exists(event)) {
+            var func = event_listeners[event];
+            func();
+        }
+    }
+}
 
 enum Tag {
     Health;
@@ -52,16 +95,21 @@ class Entity {
 
 class Test {
     static function main() {
+        var game = new Game();
+        var minionFunc = function() {
+            trace('minionFunc triggered!');
+            return EndTurn;
+        };
+        game.on_event(TurnEnded, minionFunc);
+        game.do_action(EndTurn);
+        
         var unicorn = new Entity('Unicorn', [
             Health => 6,
             Attack => 1,
             Healing => 1,
             CanAttack => 1
         ]);
-        // unicorn.set_tag(Health, 6);
-        // unicorn.set_tag(Attack, 1);
-        // unicorn.set_tag(Healing, 1);
-        // unicorn.set_tag(CanAttack, 1);
+
         damage(unicorn, 2);
         unicorn.listTags();
         trace('--> Can attack: ${unicorn.get_bool_tag(CanAttack)}');
@@ -70,8 +118,6 @@ class Test {
             Health => 2,
             Attack => 2
         ]);
-        // troll.set_tag(Health, 2);
-        // troll.set_tag(Attack, 2);
         
         var entities = [unicorn, troll];
 
